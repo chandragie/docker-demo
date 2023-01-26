565cc869bf9fdba730e07b832d666410502a4279
chandragie <chandragie@gmail.com>
2023-01-26 19:04:59 +0700

    Docker Demo Project

---
+++ src/main/java/com/docker/demo/DockerDemoApplication.java
@@ -0,0 +1,27 @@
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+@RestController
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> helloWorldController() {
+		log.info("Hey it's the HelloWorld endpoint!");
+		return new ResponseEntity<>("Hey! It works!", HttpStatus.OK);
+	}
+
+}
